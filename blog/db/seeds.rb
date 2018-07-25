bb9902b99fc8c3bb2fd692227e67effb30209133
Mai Lan Peng <mailan.peng@gmail.com>
2018-07-26 01:52:54 +0200

    ajout de commentaires

--- blog/db/seeds.rb
+++ blog/db/seeds.rb
@@ -1,18 +1,18 @@
 
 require 'faker'
 
-5.times do
+5.times do     # On génère 5 catégories avec le style Pokémon Faker
   Category.create(name: Faker::Pokemon.name)
 end
 
-10.times do
+10.times do     # On génère 10 utilisateurs avec le style gem Faker
   new_user = User.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email
   )
 
-  Article.create(
+  Article.create(   # On génère 10 articles avec le style geek de gamer WorldOfWarcraft
     title: Faker::WorldOfWarcraft.quote,
     content: Faker::Lorem.paragraph_by_chars,
     user_id: new_user.id,
@@ -20,7 +20,7 @@ end
   )
 end
 
-((User.first.id)..(User.last.id)).each do |user_id|
+((User.first.id)..(User.last.id)).each do |user_id|    # On génère 15 commentaires, pépère 
   15.times do
     Comment.create(
       content: Faker::WorldOfWarcraft.quote,
@@ -30,7 +30,7 @@ end
   end
 end
 
-  ((User.first.id)..(User.last.id)).each do |user_id|
+  ((User.first.id)..(User.last.id)).each do |user_id|    # On génère 15 likes parce qu'on le vaut bien ;)
     15.times do
       Like.create(
           user_id: rand((User.first.id)..(User.last.id)),
